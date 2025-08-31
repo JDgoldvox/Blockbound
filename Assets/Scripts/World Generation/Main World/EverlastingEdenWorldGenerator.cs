406e0a06b790289ede1e84cf216abe3c90ba6abf
JDgoldvox <voxcastjdgoldvox@gmail.com>
2025-08-31 16:06:43 +0100

    updated to 6000.2

--- Assets/Scripts/World Generation/Main World/EverlastingEdenWorldGenerator.cs
+++ Assets/Scripts/World Generation/Main World/EverlastingEdenWorldGenerator.cs
@@ -36,6 +36,11 @@ public class EverlastingEdenWorldGenerator : MonoBehaviour
 
     public void GenerateWorldParallel()
     {
+        //random struct
+        System.Random baseSeedGenerator = new System.Random();
+        uint seed = (uint)baseSeedGenerator.Next(1, int.MaxValue) | 1;
+        Unity.Mathematics.Random rng = new Unity.Mathematics.Random(seed);
+        
         _totalBlocks = _width * _height;
         
         //stuff to do before
@@ -48,7 +53,7 @@ public class EverlastingEdenWorldGenerator : MonoBehaviour
         GenerateStoneLayer(tileTypeMap);
         
         //Generate Ore
-        GenerateOreLayer(tileTypeMap);
+        GenerateOreLayer(tileTypeMap, ref rng);
         
         //main thread map build
         BuildTileMap(tileTypeMap);
@@ -148,81 +153,79 @@ public class EverlastingEdenWorldGenerator : MonoBehaviour
         }
     }
     
-    private void GenerateOreLayer(NativeArray<int> tileTypeMap)
+    private void GenerateOreLayer(NativeArray<int> tileTypeMap, ref Unity.Mathematics.Random rng)
     {
-        System.Random baseSeedGenerator = new System.Random();
-        uint seed = (uint)baseSeedGenerator.Next(1, int.MaxValue) | 1;
-        var rng = new Unity.Mathematics.Random(seed);
         int halfWidth = _width / 2;
         
         if (_everlastingEdenOreSO.EnableCopper)
         {
             for (int i = 0; i < _everlastingEdenOreSO.CopperLargeVeinQuantity; i++)
             {
-                Queue<Vector2Int> tileQueue = new Queue<Vector2Int>();
-                HashSet<Vector2Int> visitedTiles = new HashSet<Vector2Int>();
-                
                 //generate random start position within the layer
                 int rngX = rng.NextInt(-halfWidth, halfWidth);
                 int rngY = rng.NextInt(0, _height);
                 Vector2Int startPosition = new Vector2Int(rngX, rngY);
-                tileQueue.Enqueue(startPosition);
-                int tileCount = 0;
-                
-                while (tileQueue.Count != 0) //start flood fill algorithm
-                {
-                    Vector2Int tilePos = tileQueue.Dequeue();
-                    
-                    //if this position is not stone , continue, skip
-                    if (!TilemapUtils.IsSpecificTile(tilePos, 1, tileTypeMap, _width))
-                    {
-                        continue;
-                    }
-                    
-                    //if reached max tiles for this ore vein, return
-                    tileCount++;
-                    visitedTiles.Add(tilePos);
-                    tileTypeMap[TilemapConverter.CoordToIndex(tilePos, _width)] = 2;
 
-                    if (tileCount > 20)
-                    {
-                        break;
-                    }
-                    
-                    //queue adjacent tiles that are stone
-                    List<Vector2Int> adacentStoneTiles = new List<Vector2Int>();
-                    adacentStoneTiles = TilemapUtils.ReturnSpecificAdjacentTiles(
-                        startPosition,
-                        1,
-                        tileTypeMap,
-                        _height,
-                        _width);
-                
-                    //queue adjacent tiles, if not already in queue
-                    foreach (var newTilePos in adacentStoneTiles)
-                    {
-                        //rng if this tile should exist
-                        float chance = rng.NextFloat(0, 1);
-                        
-                        if (chance < _everlastingEdenOreSO.CopperLargeVeinSpreadChance)
-                        {
-                            continue;
-                        }
-
-                        if (visitedTiles.Contains(newTilePos))
-                        {
-                            continue;
-                        }
-                        
-                        tileQueue.Enqueue(newTilePos);
-                    }
-                }
+                FloodFill(startPosition, tileTypeMap, ref rng);
             }
         }
     }
 
-    private void FloodFill()
+    private void FloodFill(Vector2Int startPosition, NativeArray<int> tileTypeMap, ref Unity.Mathematics.Random rng)
     {
+        Queue<Vector2Int> tileQueue = new Queue<Vector2Int>();
+        HashSet<Vector2Int> visitedTiles = new HashSet<Vector2Int>();
         
+        tileQueue.Enqueue(startPosition);
+        int tileCount = 0;
+        
+        while (tileQueue.Count != 0) //start flood fill algorithm
+        {
+            Vector2Int tilePos = tileQueue.Dequeue();
+                    
+            //if this position is not stone , continue, skip
+            if (!TilemapUtils.IsSpecificTile(tilePos, 1, tileTypeMap, _width))
+            {
+                continue;
+            }
+                    
+            //if reached max tiles for this ore vein, return
+            tileCount++;
+            visitedTiles.Add(tilePos);
+            tileTypeMap[TilemapConverter.CoordToIndex(tilePos, _width)] = 2;
+
+            if (tileCount == 20)
+            {
+                break;
+            }
+                    
+            //queue adjacent tiles that are stone
+            List<Vector2Int> adacentStoneTiles = new List<Vector2Int>();
+            adacentStoneTiles = TilemapUtils.ReturnSpecificAdjacentTiles(
+                startPosition,
+                1,
+                tileTypeMap,
+                _height,
+                _width);
+                
+            //queue adjacent tiles, if not already in queue
+            foreach (var newTilePos in adacentStoneTiles)
+            {
+                //rng if this tile should exist
+                float chance = rng.NextFloat(0, 1);
+                        
+                if (chance < _everlastingEdenOreSO.CopperLargeVeinSpreadChance)
+                {
+                    continue;
+                }
+
+                if (visitedTiles.Contains(newTilePos))
+                {
+                    continue;
+                }
+                        
+                tileQueue.Enqueue(newTilePos);
+            }
+        }
     }
 };
